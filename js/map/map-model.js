0cca3b442c1e78883c26ec4e596868a8acb69164
Mathieu Le Roi <mathleroi@gmail.com>
2019-07-01 11:11:26 +0200

    Map implementation with position memorized

--- js/map/map-model.js
+++ js/map/map-model.js
@@ -1,72 +1,22 @@
+/**
+ * Map model used to memorize map current data into local storage
+ */
 define(function (require) {
 
     "use strict";
 
     var Backbone = require('backbone');
     var Config = require( 'root/config' );
-    var $ = require( 'jquery' );
-    var L = require( 'theme/leaflet/leaflet' );
-    //require('localstorage');
+    require('localstorage');
 
-    return Backbone.Model.extend({
-        localStorage: new Backbone.LocalStorage( 'Map-' + Config.app_slug  ),
-
+    var MapModel = Backbone.Model.extend({
+        localStorage: new Backbone.LocalStorage( "Map-" + Config.app_slug ),
         defaults: {
             id : "",
-            map: null,
             center: { lat: 45.7640, lng: 4.8357 },
-        },
-
-        init: function () {
-            if ( !this.is_map_active() && $('#'+this.get('id')).length ) {
-
-                //Clear all previous map instances:
-                this.remove();
-
-                var center = [this.get('center').lat, this.get('center').lng];
-                this.set('map', L.map( this.get('id') ).setView( center, 13 ) );
-                L.tileLayer( 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 19,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                } ).addTo( this.get('map') );
-                //this.marker = L.marker( center ).addTo( this.get('map') );
-
-                var _this = this;
-                this.get('map').on('unload', function(e){
-                    var current_center = _this.get('map').getCenter();
-                    _this.set('center',{ lat: current_center.lat, lng: current_center.lng })
-                    console.log('Unload!',_this.get('center'));
-                });
-            }
-        },
-
-        update: function () {
-
-            if ( !this.is_map_active() ) {
-                this.init();
-            }
-
-            var newLatLng = new L.LatLng( this.get('center').lat, this.get('center').lng );
-            this.get('map').panTo( newLatLng );
-            //this.marker.setLatLng( newLatLng );
-        },
-
-        remove: function() {
-
-            var $map = $('#'+this.get('id'));
-            if ( $map.length && $map.html().length ) {
-                this.get('map').remove();
-                $map.html('');
-            }
-
-            this.set('map', null);
-            //this.marker = null;
-        },
-
-        is_map_active: function() {
-            var $map = $('#'+this.get('id'));
-            return this.get('map') !== null && $map.length && $map.html().length;
+            zoom: 13
         }
     });
 
+    return MapModel;
 });
--- js/map/map-run.js
+++ js/map/map-run.js
@@ -1,12 +1,15 @@
 define([
     'jquery',
     'core/theme-app',
-    'theme/js/map/map-model'
-    ], function($,App,MapModel) {
+    'theme/js/map/map-engine-leaflet'
+    ], function($,App,Map) {
 
-        var MyMap = new MapModel({
+        var MyMap = new Map({
             id:"map",
-            center: { lat: 45.7640, lng: 4.8357 }
+            default_data: {
+                center: { lat: 45.7640, lng: 4.8357 },
+                zoom: 6
+            }
         });
 
         App.addCustomRoute( 'map', 'map' );
@@ -17,12 +20,15 @@ define([
                 $('#map').height( $('#map-screen').height() );
                 MyMap.update();
             }
+
         });
 
         App.on( 'screen:leave', function ( current_screen, queried_screen  ) {
+
             if ( current_screen.item_id === 'map' ) {
                 MyMap.remove();
             }
+
         } );
 
 });
