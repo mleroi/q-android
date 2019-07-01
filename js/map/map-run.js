fb5bb749419bd7de2411a8c0b2cf55a2ef281956
Mathieu Le Roi <mathleroi@gmail.com>
2019-07-01 11:21:21 +0200

    Comments and small adjustments

--- js/map/map-run.js
+++ js/map/map-run.js
@@ -1,9 +1,14 @@
+/**
+ * Plug map logic into the app.
+ * (Require this map-run.js file in functions.js)
+ */
 define([
     'jquery',
     'core/theme-app',
     'theme/js/map/map-engine-leaflet'
     ], function($,App,Map) {
 
+        //Initialize leaflet map object with default values
         var MyMap = new Map({
             id:"map",
             default_data: {
@@ -12,17 +17,19 @@ define([
             }
         });
 
+        //Create a custom screen for our map, with id "map" and template "map.html":
         App.addCustomRoute( 'map', 'map' );
 
+        //Display our map when map screen is showed:
         App.on( 'screen:showed', function( current_screen ) {
 
             if ( current_screen.item_id === 'map' ) {
-                $('#map').height( $('#map-screen').height() );
                 MyMap.update();
             }
 
         });
 
+        //Unload the map when we leave the map screen:
         App.on( 'screen:leave', function ( current_screen, queried_screen  ) {
 
             if ( current_screen.item_id === 'map' ) {
